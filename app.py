5486dd9befbf15c080d796811638d39adaabf03e
iamchu <v235711@gmail.com>
2017-12-31 04:22:11 -0200

    improve text printing

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# * resolver bug que pega tema e titulo como mesma coisa 
+# pegar o parent do teaser-title e do title, fica mais facil de manipular
+
 import os
 import requests
 import bs4
@@ -14,13 +17,18 @@ def getTitles():
 
 	valor_page = bs4.BeautifulSoup(valor_page.text)
 
-	titles = valor_page.find_all("div",class_="teaser")
-	if titles == []:
+	teaser_titles = valor_page.find_all("div",class_="teaser-title")
+	teasers = valor_page.find_all("div",class_="teaser")
+	if teaser_titles == []:
 		print 'no titles found'
 	else:
-		for i in range(len(titles)):
-			print titles[i]
-			print '\n'
+		for i in range(len(teaser_titles)):
+			print  '[' + teaser_titles[i].get_text().strip() + ']'
+			if teasers[i]:
+				print teasers[i].get_text().strip()
+			print '-----'*3
+		# for i in range(len(teasers)):
+			# print teasers[i].get_text()
 
 	# print valor_page.text
 
